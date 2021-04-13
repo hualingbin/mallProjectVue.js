1354217bd4acf6d4bd110d38cbd978c2b0841f9c
hualingbin <huabin4678243@gmail.com>
2021-04-13 19:58:56 +0900

    增加登陆画面

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="signin-form">
+        <h3 class="sign-title">ticket-sys 登录</h3>
+        <div>
+            <el-form :model="loginForm" :rules="rules" status-icon ref="ruleForm" class="demo-ruleForm">
+                <el-form-item prop="username">
+                    <el-input
+                            v-model="loginForm.username"
+                            autocomplete="off"
+                            placeholder="用户名"
+                            prefix-icon="el-icon-user-solid"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input
+                            type="password"
+                            v-model="loginForm.password"
+                            autocomplete="off"
+                            placeholder="请输入密码"
+                            prefix-icon="el-icon-lock"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm" id="login-btn">登录</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Login'
+    }
+</script>
