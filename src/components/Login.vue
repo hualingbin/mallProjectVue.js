d9e2062b2eadf146a753525f46a6b562e60543af
hualingbin <huabin4678243@gmail.com>
2021-04-18 11:17:17 +0900

    修改登陆画面，导入element-ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,15 +12,7 @@
                     username:'',
                     password:''
                 },
-                userToken:'',
-                rules:{
-                    username:[
-                        { required: true, message: '请输入用户名', trigger: 'blur' },
-                    ],
-                    password:[
-                        { required: true, message: '请输入密码', trigger: 'blur' },
-                    ]
-                }
+                userToken:''
             }
         },
         methods: {
