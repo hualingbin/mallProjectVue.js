da28630a643ef787593568c2412d91035d0f59de
hualingbin <huabin4678243@gmail.com>
2021-04-21 16:59:57 +0900

    增加商城网站的index页面

---
+++ src/components/webHead.vue
@@ -0,0 +1,28 @@
+<!-- 网站header部分-->
+<template>
+    <div>
+        <div class="top"></div>
+        <div class="menu">
+            <ul>
+                <li><router-link to="/">首页</router-link></li>
+                <li>全部商品</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    /**设置ul */
+    ul{margin: 0px;padding: 0px;list-style-type: none;}
+    /**设置顶部区域 */
+    .top{width: 100%;height: 58px;background: #000;display: flex;align-items: center;}
+    /**设置菜单栏 */
+    .menu{width: 100%;height: 74px;background: #fff;}
+    /**设置菜单栏下的ul li*/
+    .menu ul{width:1180px;display: flex;margin: 0 auto;}
+    .menu ul li{flex:1;text-align: center;line-height: 74px;}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
-import Login from '@/components/login'
+/**引入index页面 */
+import index from '@/views/index'
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/login',
-        name: 'login',
-        component: Login
+        path: '/',
+        name: 'index',
+        component: index
     }]
 })
