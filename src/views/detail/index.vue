a64cf7747d1a7e3bdebc8b010d79464d9d5c42fd
hualingbin <huabin4678243@gmail.com>
2021-04-27 14:42:13 +0900

    格式化详情页

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -1,30 +1,29 @@
 <template>
-    <div>
-        <h1>详情页面</h1>
-    </div>
+  <div>
+    <h1>详情页面</h1>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            goodsId:''
-        }
+  data() {
+    return {
+      goodsId: "",
+    };
+  },
+  created() {
+    //获取查询参数
+    this.goodsId = this.$route.query.goodsId;
+    console.log(this.$route.query.goodsId);
+    this.getData();
+  },
+  methods: {
+    getData() {
+      this.$axios.get("http//.....").then((res) => {
+        //获得商品详细信息
+      });
     },
-    created(){
-        //获取查询参数
-        this.goodsId = this.$route.query.goodsId
-        console.log(this.$route.query.goodsId)
-        this.getData()
-    },
-    methods:{
-        getData(){
-            this.$axios.get('http//.....').then(res=>{
-                //获得商品详细信息
-            })
-        }
-    }
-}
+  },
+};
 </script>
 <style scoped>
-
 </style>
